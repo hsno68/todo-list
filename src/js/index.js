de834b7334fd579034eb0b9c24708f970235882a
haiseno <haise.zhao@gmail.com>
2025-05-01 01:11:00 -0700

    Add buttons to separate functionality

--- src/js/index.js
+++ src/js/index.js
@@ -5,33 +5,39 @@ import addTaskItem from "./taskAdder.js";
 import makeProject from "./projectMaker.js";
 import addProject from "./projectAdder.js";
 
-const button = document.querySelector("button");
-if (button) {
-  button.addEventListener("click", updateState);
+const addProjectButton = document.querySelector("button.addProject");
+const addTaskButton = document.querySelector("button.addTask");
+if (addProjectButton) {
+  addProjectButton.addEventListener("click", updateProjectCollection);
+}
+if (addTaskButton) {
+  addTaskButton.addEventListener("click", updateTaskState);
 }
 
 const projectsCollection = {
   allProjectIds: []
 };
 
-function updateState() {
-  let projectId = generateId();
+let projectId;
+
+function updateProjectCollection() {
+  projectId = generateId();
   //Regenerate a new project Id if project Id already exists
   while (projectsCollection.hasOwnProperty(projectId)) {
     projectId = generateId();
   }
   const newProject = makeProject(prompt("Enter a project title"));
   addProject(projectId, projectsCollection, projectsCollection.allProjectIds, newProject);
+  console.log({projectsCollection});
+}
 
+function updateTaskState() {
   const taskItem = new TaskMaker(
     prompt("Name of Task"),
     prompt("Description"),
     prompt("Due"),
     prompt("Priority"),
-    prompt("Notes"),
-    prompt("Status")
   );
-
   addTaskItem(projectsCollection[projectId].tasksByIds, projectsCollection[projectId].allTaskIds, taskItem)
   console.log({projectsCollection});
 }
