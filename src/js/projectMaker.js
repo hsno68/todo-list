8a872ff49615a0d0091554ca32fe1e4461279887
haiseno <haise.zhao@gmail.com>
2025-05-08 11:22:42 -0700

    Rename properties

--- src/js/projectMaker.js
+++ src/js/projectMaker.js
@@ -1,7 +1,7 @@
 export default class ProjectMaker {
   constructor(title) {
     this.title = title;
-    this.todosByIds = {};
-    this.allTodoIds = [];
+    this.todosByIdsObject = {};
+    this.allTodoIdsArray = [];
   }
 }
