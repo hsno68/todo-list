d94cd02f330b35a4d5c1632445e71a13edcd0e66
haiseno <haise.zhao@gmail.com>
2025-05-29 16:48:17 -0700

    Update to use public API on project manager

--- src/js/init/setupProject.js
+++ src/js/init/setupProject.js
@@ -3,10 +3,8 @@ import projectManager from "./../app/projectManager.js";
 import { defaultProject } from "./../app/projectMaker.js";
 
 export default function setupProject() {
+  const { projectForm } = getDOMElements();
   projectManager.add(defaultProject);
-
-  const { projectsContainer, projectForm } = getDOMElements();
-
-  projectsContainer.appendChild(defaultProject.element);
+  projectManager.render();
   projectForm.setAttribute("data-project-id", defaultProject.id);
 }
